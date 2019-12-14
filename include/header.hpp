4f7a7bc30f0b6e5ba7952a1fa6c7f0cdd2002fbb
ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-13 17:33:35 -0800

    0.2

--- include/header.hpp
+++ include/header.hpp
@@ -1,20 +1,19 @@
 // Copyright 2018 Your Name <your_email>
 
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
 #include <iostream>
 #include <boost/filesystem.hpp>
 #include <map>
 #include <vector>
+#include <string>
 
 #define DIRECTORY 3
 #define COM_FILE 2
 #define RIGHT_LENGTH 29
 
-using namespace boost::filesystem;
+using boost::filesystem::path;
+
 using std::cout;
 using std::endl;
-using std::string;
 using std::map;
 using std::vector;
 using std::stoi;
@@ -24,14 +23,8 @@ using boost::filesystem::directory_iterator;
 class fsystem
 {
 public:
-    fsystem(path input_dir)
-    {
 
-        //_input_dir = *input_dir;
-        //cout << _input_dir;
-    }
-
-    bool is_digit(string verifiable){
+    bool is_digit(std::string verifiable){
         bool flag = true;
         for(int i = 0; i < 8; ++i)
         {
@@ -54,7 +47,7 @@ public:
     bool check_format(const path file_n)
     {
         bool flag = false;
-        string file = file_n.c_str();
+        std::string file = file_n.c_str();
 
         if((file.length() == RIGHT_LENGTH) &&
         (file.substr(0,8) == "balance_") &&
@@ -67,19 +60,29 @@ public:
     }
 
     void create_account(path _path) {
-        string __path = _path.filename().c_str();
-        string __parent_name = _path.parent_path().filename().c_str();
+        std::string __path = _path.filename().c_str();
+        std::string __parent_name = _path.parent_path().filename().c_str();
 
-        vector<string> tmp;
+        vector<std::string> tmp;
         tmp.push_back(__parent_name);
         tmp.push_back(__path.substr(8, 8));
         tmp.push_back(__path.substr(17, 8));
         broker_map.push_back(tmp);
     }
-/*
+
+    void print_second()
+    {
+        for (int i = 0; i < clear_map.size(); ++i)
+            cout << "broker:" << clear_map[i][0] <<
+                 " account:" << clear_map[i][1] <<
+                 " files:" << clear_map[i][2] <<
+                 " lastdate:" << clear_map[i][3]
+                 << endl;
+
+    }
+
     void create_clear()
     {
-        //vector<vector<string>> clear_map;
         for (int it = 0; it < broker_map.size(); it++)
         {
             int j = 0;
@@ -90,31 +93,35 @@ public:
                 if((broker_map[it][0] == clear_map[j][0]) &&
                 (broker_map[it][1] == clear_map[j][1])) {
                     flag = 1;
-                    clear_map[j][2] += 1;
+                    int tmp = stoi(clear_map[j][2]);
+                    tmp++;
+                    clear_map[j][2] = std::to_string(tmp);
                     if (stoi(broker_map[it][2]) > stoi(clear_map[j][3])) {
+
                         clear_map[j][3] = broker_map[it][2];
                     }
                 }
             }
             if (flag == 0) {
-                vector<string> tmp;
+                vector<std::string> tmp;
                 tmp.push_back(broker_map[it][0]);
                 tmp.push_back(broker_map[it][1]);
                 tmp.push_back("1");
+                tmp.push_back(broker_map[it][2]);
                 clear_map.push_back(tmp);
             }
         }
     }
-*/
 
     void start(path _input_dir)
     {
         parent_dir = _input_dir;
         engine(_input_dir);
         print_first();
-        cout << "HELLO";
-        //create_clear();
-        //print_second();
+
+        vector<vector<std::string>> a;
+        create_clear();
+        print_second();
     }
 
 
@@ -127,16 +134,14 @@ public:
                 if(check_format(p->path().filename()) == true)
                 {
                     create_account(p->path());
-                    //cout << p->path().parent_path().filename()<< "  " << p->path().filename() << endl;
                 }
             }
         }
     }
 
     path parent_dir;
-    vector<vector<string>> clear_map;
-    //string _input_dir;
-    vector<vector<string>> broker_map;
+    vector<vector<std::string>> clear_map;
+    vector<vector<std::string>> broker_map;
 };
 
 #endif // INCLUDE_HEADER_HPP_
