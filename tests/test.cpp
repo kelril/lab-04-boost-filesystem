8ad559e32d8450c585893cdee656758de2f3fba8
ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-20 23:09:13 -0800

    final

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,12 +6,12 @@
 
 TEST(Example, EmptyTest) {
     auto curr_path = boost::filesystem::current_path();
-    string str_curr_path = curr_path.string();
-    if (str_curr_path.find("sources") != string::npos)
+    std::string str_curr_path = curr_path.string();
+    if (str_curr_path.find("sources") != std::string::npos)
     {
         str_curr_path.erase(str_curr_path.rfind("/"),
                         str_curr_path.length() - 1);
-        str_curr_path += string("/misc/ftp");
+        str_curr_path += std::string("/misc/ftp");
     }
     auto path_ftp = boost::filesystem::path{str_curr_path.c_str()};
     fsystem result;
