c1321ac14f2a8ff1c3874a271588a2e142f62722
ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-20 23:02:09 -0800

    final

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,21 +3,23 @@
 #include <gtest/gtest.h>
 #include <header.hpp>
 
+
 TEST(Example, EmptyTest) {
     auto curr_path = boost::filesystem::current_path();
-    std::string str_path = curr_path.c_str();
-
-    str_path.erase(str_path.rfind("/"),
-    str_path.length() - 1);
-    str_path += std::string("/misc/ftp");
-    fsystem test;
-    test.start(str_path.c_str());
-    EXPECT_EQ(test.number_of("bcs", 122223), 46);
+    string str_curr_path = curr_path.string();
+    if (str_curr_path.find("sources") != string::npos)
+    {
+        str_curr_path.erase(str_curr_path.rfind("/"),
+                        str_curr_path.length() - 1);
+        str_curr_path += string("/misc/ftp");
+    }
+    auto path_ftp = boost::filesystem::path{str_curr_path.c_str()};
+    my_FTP result(path_ftp);
+    EXPECT_TRUE(true);
 }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
 
