61c0f677ddf93bac460cccc295190eee78fd0cb8
ogrebatel <kotkov.oleg.2011@yandex.ru>
2019-12-20 22:25:42 -0800

    final

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,8 +4,11 @@
 #include <header.hpp>
 
 TEST(Example, EmptyTest) {
+    auto curr_path = boost::filesystem::current_path();
+    std::string str_path = curr_path.c_str();
+    str_path += string("misc/ftp");
     fsystem test;
-    test.start("./../misc/ftp/");
+    test.start(str_path.c_str());
     EXPECT_EQ(test.number_of("bcs", 122223), 46);
 }
 
